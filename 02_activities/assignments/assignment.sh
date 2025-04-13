9dfc66baae065459152dc8587c7353c68959bd9f
basaberi6 <babak.s.747@gmail.com>
2025-04-13 19:14:32 -0400

    finished the assignment

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -51,7 +51,7 @@ rm ./data/raw/*ipaddr
 rm ./data/processed/user_logs/*ipaddr 
 
 # 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
-ls -rl ./data/processed > ls-./data/inventory.txt
+ls -r ./data/processed > ls-./data/inventory.txt
 
 ###########################################
 #added code to upload to github
