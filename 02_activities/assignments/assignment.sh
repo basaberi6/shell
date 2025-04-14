57dd1683ca6c7f46e99d9a2b9009262958e49591
basaberi6 <babak.s.747@gmail.com>
2025-04-13 21:39:06 -0400

    finished the assignment

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-find . ! -name 'assignment.sh' -type f -exec rm -f {} +
+#find . ! -name 'assignment.sh' -type f -exec rm -f {} +
 
 set -x
 
@@ -28,7 +28,7 @@ mkdir data
 mkdir data/raw
 
 # 2. Move the ./rawdata directory to ./data/raw
-mv ./rawdata/ ./data/raw 
+mv ./rawdata/* ./data/raw/
 
 # 3. List the contents of the ./data/raw directory
 ls ./data/raw
@@ -40,11 +40,11 @@ mkdir data/processed/user_logs
 mkdir data/processed/event_logs
 
 # 5. Copy all server log files (files with "server" in the name AND a .log extension) from ./data/raw to ./data/processed/server_logs
-cp ./data/raw/rawdata/*server*.log ./data/processed/server_logs/
+cp ./data/raw/*server*.log ./data/processed/server_logs/
 
 # 6. Repeat the above step for user logs and event logs
-cp ./data/raw/rawdata/*user*.log ./data/processed/user_logs/
-cp ./data/raw/rawdata/*event*.log ./data/processed/event_logs/
+cp ./data/raw/*user*.log ./data/processed/user_logs/
+cp ./data/raw/*event*.log ./data/processed/event_logs/
 
 # 7. For user privacy, remove all files containing IP addresses (files with "ipaddr" in the filename) from ./data/raw and ./data/processed/user_logs
 #rm ./data/raw/*ipaddr
